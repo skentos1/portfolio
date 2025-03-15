ad29a862ed78990b16b2c6f68f37d8015e536243
skentos1 <kentos.simon@gmail.com>
2025-03-15 20:10:36 +0100

    eslint fixess

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // Zakážeme pravidlá, ktoré nechceme (alebo ich nastavíme na warn)
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
